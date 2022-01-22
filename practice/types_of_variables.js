73fa2e8888d8a98dfaedeb7388ddc992e2ff398d
Rajat Upadhyay <43850934+rajatmw1999@users.noreply.github.com>
2022-01-22 18:26:33 +0530

    Create types_of_variables.js

---
+++ practice/types_of_variables.js
@@ -0,0 +1,12 @@
+There are three types of variables in JS
+
+1. var
+2. let
+3. const
+
+var - no errors on mistakes, global scope - exists throughout the program 
+let - we get errors on mistakes, block scope - only exists in the place where is it defined, not outside of it 
+const - a constant variable that cannot be reassigned another value 
+
+https://www.w3schools.com/js/js_let.asp
+https://www.w3schools.com/js/js_const.asp
