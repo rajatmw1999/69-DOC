72501302802ce7f760fd9bd0f426a0916613f21a
RAJAT UPADHYAY ENCY <rajatis1999@gmail.com>
2022-01-26 18:26:48 +0530

    added more topics

--- commit.sh
+++ commit.sh
@@ -1,4 +1,12 @@
+git pull origin master
 git add .
 git commit -m "added more topics"
 git push origin master
-sleep 2
+echo "=================="
+echo "=================="
+echo "=================="
+echo "   Done!!!   "
+echo "=================="
+echo "=================="
+echo "=================="
+sleep 5
