3bb29c556f0443a2600543c753c9df9986fb9322
RAJAT UPADHYAY ENCY <rajatis1999@gmail.com>
2022-03-15 18:30:35 +0530

    added more topics

---
+++ code/d_javascript_essentials/functions/script.js
@@ -0,0 +1,13 @@
+// console.log('Functions Basics and Advanced.')
+
+// 1. Either i want to execute code again and again
+// 2. Or I want to write code here but exeute that code somewhere else
+
+// var myClass = 10
+
+function shouryaisboss () {
+  console.log('This is my first function.')
+  let marks1 = 56
+  let marks2 = 78
+  console.log('Total marks = ', marks1 + marks2)
+}
